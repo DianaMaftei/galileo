19327f55ea5ed6c8dc54b2b21d9e575754ed343e
dianaMaftei <dmaftei@bitbucket.endava.com>
2019-06-17 19:30:37 +0300

    add option find your way around

--- app/src/main/java/com/endava/myapplication/MainActivity.java
+++ app/src/main/java/com/endava/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.endava.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
         Intent i = new Intent(MainActivity.this, MyIntro.class);
         startActivity(i);
+
+        LinearLayout menu_photos = findViewById(R.id.findWayAroundMainOption);
+        menu_photos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("cliiiick");
+                Intent picture_intent = new Intent(MainActivity.this, MainOptionsActivity.class);
+                startActivity(picture_intent );
+            }
+        });
 
     }
 }
